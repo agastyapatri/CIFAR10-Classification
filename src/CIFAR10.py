8397f2c8f2289eb9b63bcbbb81d750c6494641aa
Agastya Patri <87602472+agastyapatri@users.noreply.github.com>
2022-06-12 18:45:32 +0530

    Update CIFAR10.py

--- src/CIFAR10.py
+++ src/CIFAR10.py
@@ -6,7 +6,7 @@ import numpy as np
 import torch.nn as nn
 import matplotlib.pyplot as plt
 import pandas as pd
-import torch.nn.functional as functional
+from torch.utils import tensorboard
 from torchvision import transforms
 import os
 
@@ -22,17 +22,18 @@ class Model(nn.Module):
     The model which will be trained and tested
     """
 
-    def __init__(self, input_size, output_size, learning_rate, momentum, epochs, batches):
+    def __init__(self, input_size, output_size, learning_rate, momentum, num_epochs, batches, weightdecay):
         super().__init__()
 
         self.input_size = input_size
         self.output_size = output_size
         self.learning_rate = learning_rate
         self.momentum = momentum
-        self.epochs = epochs
+        self.epochs = num_epochs
         self.criterion_1 = nn.CrossEntropyLoss()
         self.criterion_2 = nn.NLLLoss()
         self.batches = batches
+        self.weightdecay = weightdecay
 
     """-----------------------------------------------------------------------------------------------------------------
     2. Visualizing the Data 
@@ -47,7 +48,7 @@ class Model(nn.Module):
         images, labels = dataiter.next()
 
         for i in range(idx):
-            plt.subplot(idx, idx, i+1)
+            plt.subplot(1, idx, i+1)
             plt.imshow(images[i][1,:,:])
             plt.show()
 
@@ -57,7 +58,7 @@ class Model(nn.Module):
 
     def network(self, loader):
         """
-        Defining the CNN that will be trained
+        Defining the NN that will be trained
         """
         dataiter = iter(loader)
         tensor, labels = dataiter.next()
@@ -65,19 +66,21 @@ class Model(nn.Module):
         # input is now shaped [1, 3, 1024]
         input = torch.flatten(tensor, 1)
 
-        hidden_sizes = [1024, 64]
+        hidden_sizes = [1024, 128, 64]
 
         model = nn.Sequential(
             # Input Layer
             nn.Linear(self.input_size, hidden_sizes[0]),
             nn.ReLU(),
-            # Hidden Layer
-            nn.Linear(hidden_sizes[0], hidden_sizes[1]),
+            # Hidden Layer 1
+            nn.Linear(hidden_sizes[0], hidden_sizes[2]),
             nn.ReLU(),
-            # Output Lauer
-            nn.Linear(hidden_sizes[1], self.output_size),
+
+            # Hidden Lauer 3
+            nn.Linear(hidden_sizes[2], self.output_size),
             nn.LogSoftmax(dim=1)
         )
+
         # The shape of the predictions are [1 x 10]
         predictions = model(input)
         return predictions, model
@@ -88,48 +91,69 @@ class Model(nn.Module):
 
     def train_model(self, loader):
         """
-        Function to perform the training step.
+        Function to perform the training step. Also calculates the training accuracy for each epoch
         :param loader: the train loader
         """
         images, labels = next(iter(train))
-        model = self.network(train)[1]
+        model = self.network(loader)[1]
         training_loss = []
-
+        training_accuracy_array = []
         # Defining the optimizer for the model
-        optimizer = torch.optim.SGD(model.parameters(), lr=self.learning_rate, momentum=self.momentum)
+        optimizer = torch.optim.SGD(model.parameters(), lr=self.learning_rate,
+                                    momentum=self.momentum, weight_decay=self.weightdecay)
 
         # Looping over the number of epochs
         for epoch in range(self.epochs):
             running_loss = 0.0
+            correct_count = 0
+            incorrect_count = 0
 
             # Looping over all the images
             for i, datapoint in enumerate(loader):
-                image , label = datapoint
+                image , true_label = datapoint
                 inputs = torch.flatten(image, 1)
 
                 optimizer.zero_grad()
                 prediction = model(inputs)
+                predicted_labels = torch.max(prediction, 1)[1]
 
                 # Actual training pass
                 with torch.autograd.set_detect_anomaly(True):
                     # finding loss
-                    loss = self.criterion_1(prediction, label)
-
+                    loss = self.criterion_1(prediction, true_label)
                     # backpropagation
                     loss.backward(retain_graph = True)
-
                     # updating weights
                     optimizer.step()
 
-                    running_loss += loss.item()
-                    training_loss.append(running_loss/len(train))
+                running_loss += loss.item()
+
+                # Calculating the correctly and incorrectly predicted labels for training.
+                bool_array = np.array(predicted_labels == true_label)
+                num_correct, num_incorrect = np.count_nonzero(bool_array), self.batches - np.count_nonzero(bool_array)
+
+                training_loss.append(running_loss/len(train))
+                correct_count += num_correct
+                incorrect_count += num_incorrect
+
+
+
 
             else:
+                training_accuracy = correct_count / (correct_count + incorrect_count)
+                training_accuracy_array.append(training_accuracy)
 
-                print(f"Epoch {epoch} : Training Loss = {running_loss/len(train)}")
+                print(f"Epoch {epoch} : Training Loss = {running_loss/len(train)}. Training Accuracy = {round(training_accuracy*100, 6)} %")
+
         print("Training Done!")
+
+        # Creating numpy arrays for the two metrics.
         training_loss = np.array(training_loss)
-        return training_loss
+        training_accuracy_array = np.array(training_accuracy_array)
+
+        return training_loss, training_accuracy_array
+
+
 
 
     """-----------------------------------------------------------------------------------------------------------------
@@ -143,56 +167,58 @@ class Model(nn.Module):
         """
 
         images, labels = next(iter(loader))
-        predictions, model = self.network(loader)
 
+        # Calculating the number of correct and incorrect labels
         correct_count = 0
         incorrect_count = 0
 
-        # Calculating the number of correct and incorrect labels
+
         with torch.no_grad():
+            # Looping over the batches
             for i, data in enumerate(loader):
-                image, label = data
-                inputs = torch.flatten(image, 1)
-                prediction = model(inputs)
-                predicted_label = torch.max(prediction)
+                image, true_labels = data
+                flattened_image = torch.flatten(image, 1)
 
-                if predicted_label == label:
-                    correct_count += 1
-                else:
-                    incorrect_count += 1
+                model = self.network(loader)[1]
+                prediction = model(flattened_image)
+                predicted_labels = torch.max(prediction, 1)[1]
 
-        accuracy = (correct_count)/ (correct_count + incorrect_count)
+                # Bool array is an array of Truth values, and the code below counts how many are True (correctly predicted)
+                bool_array = np.array(predicted_labels == true_labels)
+                num_correct, num_incorrect = np.count_nonzero(bool_array), self.batches - np.count_nonzero(bool_array)
 
-        print(f"Accuracy of the Model = {accuracy*100}%")
-        return accuracy, correct_count, incorrect_count
+                correct_count += num_correct
+                incorrect_count += num_incorrect
+
+            accuracy = correct_count / (correct_count + incorrect_count)
+            print(f"Testing Accuracy of the Model is: {round(accuracy * 100, 6)} % ")
+            return accuracy
+
+
+
 
     """-----------------------------------------------------------------------------------------------------------------
     6. Plotting the Results
     -----------------------------------------------------------------------------------------------------------------"""
-    def results(self, training_loss):
+    def learning_curves(self, training_loss):
         """
         Function to plot the loss curves
         :param training_loss: array
         :return:
         """
-        steps_X = range(self.epochs)
-        targets_Y = training_loss
-
-        plt.title("Training Loss of the Model")
-        plt.plot(steps_X, targets_Y, "g-")
-        plt.grid()
-        plt.xlabel("Epochs / Training Steps")
-        plt.ylabel(f"Training Loss ({self.criterion_1})")
-        plt.show()
+        pass
 
 
+
+#----------------------------------------------------------------------------------------------------------------------#
 
 if __name__ == "__main__":
 
     """-----------------------------------------------------------------------------------------------------------------
     0. Preliminary Operations and Functions 
-    -----------------------------------------------------------------------------------------------------------------"""
+    -------------------------------------------------------------------Commit 2: ".gitignore" added to /data/ to ignore the large data files.----------------------------------------------"""
     PATH = "/home/agastya123/PycharmProjects/DeepLearning/CIFAR-10/"
+
 
     def load_data(option):
         """
@@ -211,7 +237,7 @@ if __name__ == "__main__":
             print("option can only be train, validation and test!")
 
         # Data has not been batched yet
-        loader = torch.utils.data.DataLoader(dataset, shuffle=True)
+        loader = torch.utils.data.DataLoader(dataset, shuffle=True, batch_size = 16)
         return loader
 
 
@@ -222,15 +248,31 @@ if __name__ == "__main__":
     -----------------------------------------------------------------------------------------------------------------"""
     train = load_data(option="train")
     validation = load_data(option="validation")
+    len_train = len(list(enumerate(train)))
+    len_val = len(list(enumerate(validation)))
 
-    model1 = Model(input_size=3072, output_size=10, learning_rate = 0.001, momentum = 0.9, epochs = 20, batches = 1)
+
+    model1 = Model(input_size=3072, output_size=10, learning_rate=1e-3, momentum=0.9, num_epochs=30, batches=16, weightdecay=1e-5)
 
 
     def RunModel():
         """
-        Runner Code to perform each step defined in the Model Class This is a test
+        Runner Code to perform each step defined in the class Model. This function also Plots the training/testing loss + accuracy
+        train_loss.shape = num_epochs * num_batches
         """
-        trained = model1.train_model(train)
+        train_loss, train_accuracy = model1.train_model(train)
         validated = model1.test_model(validation)
-        result = model1.results(trained)
+
+
+        # steps_X = np.array(range(model1.ep\ochs))
+        # train_accuracy_Y1 = train_accuracy
+        # train_loss_Y2 = train_loss
+        #
+        # # Plotting the Learning Curves of the Model
+        # fig, axs = plt.subplot_mosaic([ ["A", "C"],["B", "C"]], constrained_layout=True)
+
+
+
+
+
 
